6f793c18d15e9e97bbb08deda89a339be7f1d78b
Ronit.Mondal <Ronit.Mondal@landmarkgroup.in>
2024-08-01 08:28:31 +0530

    final_model_changes

--- app.py
+++ app.py
@@ -13,5 +13,15 @@ def train_and_log_svm():
     evaluation = model_v2.run_svm_experiment()
     return jsonify(evaluation)
 
+@app.route('/evaluate_decision_tree', methods=['GET'])
+def evaluate_decision_tree():
+    evaluation = model_v2.evaluate_saved_decision_tree()
+    return jsonify(evaluation)
+
+@app.route('/evaluate_svm', methods=['GET'])
+def evaluate_svm():
+    evaluation = model_v2.evaluate_saved_svm()
+    return jsonify(evaluation)
+
 if __name__ == '__main__':
     app.run(debug=True)
